3afe113adaffd2598ee3c56d0d73bb7fdbe836d9
Anthony <8753187+bigfoott@users.noreply.github.com>
2019-09-27 18:50:03 -0400

    font change for all fonts

--- twitch.tv.user.js
+++ twitch.tv.user.js
@@ -9,6 +9,9 @@
 GM_addStyle ( `
 .tw-root--theme-dark .channel-root__right-column {
     --color-background-body: rgb(15,12,18);
+}
+
+a, abbr, acronym, address, applet, article, aside, audio, b, big, blockquote, body, canvas, caption, center, cite, code, dd, del, details, dfn, div, dl, dt, em, embed, fieldset, figcaption, figure, footer, form, h1, h2, h3, h4, h5, h6, header, hgroup, html, i, iframe, img, ins, kbd, label, legend, li, mark, menu, nav, object, ol, output, p, pre, q, ruby, s, samp, section, small, span, strike, strong, sub, summary, sup, table, tbody, td, tfoot, th, thead, time, tr, tt, u, ul, var, video {
     font-family: Arial, sans-serif !important;
 }
 
@@ -35,4 +38,8 @@ body {
 .side-nav {
     --color-background-alt: rgb(23,20,31);
 }
-` );
+
+.channel-header-verified-badge, .tw-root--theme-dark .channel-header-verified-badge, .tw-root--theme-light .channel-header-verified-badge {
+    color: rgb(100, 65, 165) !important;
+}
+` ); 
